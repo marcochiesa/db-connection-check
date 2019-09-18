d8d07f8fe1b0c0e475184031e9e7eeda88a23f8f
Marco Chiesa <getmarcochiesa@gmail.com>
2019-09-17 19:13:40 -0500

    initial files

---
+++ src/main/java/org/getmarco/dbconncheck/DbconncheckApplication.java
@@ -0,0 +1,13 @@
+package org.getmarco.dbconncheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbconncheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbconncheckApplication.class, args);
+	}
+
+}
